e5c8c6ee8f7453bb9d269cb546502cb03ed59d8c
Mario Pabon <hey@mariopabon.com>
2015-04-17 15:36:44 -0400

    Non-realtime api

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -4,34 +4,35 @@
 
   App.SearchController = Ember.ArrayController.extend({
     query: '',
-    socket: io('http://localhost:3000'),
+    tweets: [],
     happyTweets: function(){
 
       return this.filter(function(tweet){
         return tweet.isSad === false;
       });
 
-    }.property('lastTweet'),
+    }.property('tweets'),
     sadTweets: function(){
 
       return this.filter(function(tweet){
         return tweet.isSad === true;
       });
 
-    }.property('lastTweet'),
+    }.property('tweets'),
     lastTweet: 0,
     actions: {
       search: function(){
 
-        this.set('model', []);
-
-        this.get('socket').emit('search', this.get('query'));
+        this.set('tweets', []);
 
         var that = this;
-        this.get('socket').on('tweet', function(tweet){
-          console.log(tweet.text);
-          that.set('lastTweet', tweet);
-          that.insertAt(0, tweet);
+        $.ajax({
+          url: 'http://localhost:3000/search/' + this.get('query'),
+          jsonp: 'callback',
+          success: function(tweets){
+            that.set('model', tweets);
+            that.set('tweets', tweets);
+          }
         });
 
       }
